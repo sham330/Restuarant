baab927c7655a1f8c527ade14e1a3b08a037e801
Akshaya <akshayaaks333@gmail.com>
2025-01-30 17:12:18 +0530

    committed by mrs sham

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(2)
 
   return (
     <>
       <div>
+        <h1>Akshaya Sham</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
